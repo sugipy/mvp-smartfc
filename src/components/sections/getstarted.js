0e07be212c3ff5dd74268ba730b432d795c37aab
sugipy <hunterpresident@gmail.com>
2020-07-19 03:50:59 +0900

    secont push

--- src/components/sections/getstarted.js
+++ src/components/sections/getstarted.js
@@ -7,8 +7,23 @@ const GetStarted = () => (
   <StyledSection>
     <GetStartedContainer>
       <GetStartedTitle>フランチャイズもDXの時代へ</GetStartedTitle>
-      <TryItButton>事前登録</TryItButton>
-      <Subtitle></Subtitle>
+      <HeaderForm
+        name="early-access"
+        method="post"
+        data-netlify-honeypot="bot-field"
+        data-netlify="true"
+      >
+        <input type="hidden" name="bot-field" />
+        <input type="hidden" name="form-name" value="early-access" />
+        <HeaderInput
+          type="email"
+          placeholder="メールアドレス"
+          name="email"
+          id="email"
+          required
+        />
+        <HeaderButton>無料で事前登録</HeaderButton>
+      </HeaderForm>
     </GetStartedContainer>
   </StyledSection>
 )
@@ -67,4 +82,68 @@ const Subtitle = styled.span`
   padding-top: 16px;
   font-size: 14px;
   color: ${props => props.theme.color.primary};
+`
+
+const HeaderForm = styled.form`
+  display: flex;
+  flex-direction: row;
+  padding-bottom: 16px;
+  width: 60%;
+  @media (max-width: ${props => props.theme.screen.sm}) {
+    flex-direction: column;
+  }
+`
+const HeaderInput = styled.input`
+  font-weight: 500;
+  font-size: 16px;
+  color: ${props => props.theme.color.primary};
+  line-height: 42px;
+  width: 120%;
+  text-align: left;
+  height: 60px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: ${props => props.theme.color.secondary};
+  border-image: initial;
+  border-radius: 4px;
+  padding: 8px 16px;
+  outline: 0px;
+  &:focus {
+    box-shadow: inset ${props => props.theme.color.secondary} 0px 0px 0px 2px;
+  }
+  @media (max-width: ${props => props.theme.screen.md}) {
+    margin-bottom: 8px;
+  }
+  @media (max-width: ${props => props.theme.screen.sm}) {
+    display: block;
+    width: 100%;
+  }
+`
+const HeaderButton = styled.button`
+  font-weight: 500;
+  font-size: 14px;
+  color: white;
+  letter-spacing: 1px;
+  height: 60px;
+  display: block;
+  margin-left: 8px;
+  text-transform: uppercase;
+  cursor: pointer;
+  white-space: nowrap;
+  background: ${props => props.theme.color.secondary};
+  border-radius: 4px;
+  padding: 0px 40px;
+  border-width: 0px;
+  border-style: initial;
+  border-color: initial;
+  border-image: initial;
+  outline: 0px;
+  &:hover {
+    box-shadow: rgba(110, 120, 152, 0.22) 0px 2px 10px 0px;
+  }
+  @media (max-width: ${props => props.theme.screen.md}) {
+  }
+  @media (max-width: ${props => props.theme.screen.sm}) {
+    margin-left: 0;
+  }
 `
